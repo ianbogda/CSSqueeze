3e21cc48f25fefadd90e63675339719dfaa1a360
Yann Bogdanovic <ianbogda@gmail.com>
2012-12-17 23:24:38 +0100

    + Preserve hack comments - Remove prefix vendor

--- class/CSSqueeze.php
+++ class/CSSqueeze.php
@@ -15,6 +15,7 @@ class CSSqueeze
 {
 	protected
 
+	$keepHack       = true,
 	$units          = array('in','cm','mm','pt','pc','px','rem','em','%','ex','gd','vw','vh','vm','deg','grad','rad','ms','s','khz','hz'),
 	$prop_values    = array(
 		'background',       'background-position', 'background-size',
@@ -101,36 +102,16 @@ class CSSqueeze
 		'z-index',
 		'display',
 		'visibility',
-		'-webkit-flex-direction',
-		'-moz-flex-direction',
-		'-ms-flex-direction',
-		'-o-flex-direction',
 		'flex-direction',
-		'-webkit-flex-order',
-		'-moz-flex-order',
-		'-ms-flex-order',
-		'-o-flex-order',
 		'flex-order',
-		'-webkit-flex-pack',
-		'-moz-flex-pack',
-		'-ms-flex-pack',
-		'-o-flex-pack',
 		'flex-pack',
 		'float',
 		'clear',
-		'-webkit-flex-align',
-		'-moz-flex-align',
-		'-ms-flex-align',
-		'-o-flex-align',
 		'flex-align',
 		'overflow',
-		'-ms-overflow-x',
-		'-ms-overflow-y',
 		'overflow-x',
 		'overflow-y',
 		'clip',
-		'-webkit-box-sizing',
-		'-moz-box-sizing',
 		'box-sizing',
 		'margin',
 		'margin-top',
@@ -175,80 +156,25 @@ class CSSqueeze
 		'border-left-width',
 		'border-left-style',
 		'border-left-color',
-		'-webkit-border-radius',
-		'-moz-border-radius',
 		'border-radius',
-		'-webkit-border-top-left-radius',
-		'-moz-border-radius-topleft',
 		'border-top-left-radius',
-		'-webkit-border-top-right-radius',
-		'-moz-border-radius-topright',
 		'border-top-right-radius',
-		'-webkit-border-bottom-right-radius',
-		'-moz-border-radius-bottomright',
 		'border-bottom-right-radius',
-		'-webkit-border-bottom-left-radius',
-		'-moz-border-radius-bottomleft',
 		'border-bottom-left-radius',
-		'-webkit-border-image',
-		'-moz-border-image',
-		'-o-border-image',
 		'border-image',
-		'-webkit-border-image-source',
-		'-moz-border-image-source',
-		'-o-border-image-source',
 		'border-image-source',
-		'-webkit-border-image-slice',
-		'-moz-border-image-slice',
-		'-o-border-image-slice',
 		'border-image-slice',
-		'-webkit-border-image-width',
-		'-moz-border-image-width',
-		'-o-border-image-width',
 		'border-image-width',
-		'-webkit-border-image-outset',
-		'-moz-border-image-outset',
-		'-o-border-image-outset',
 		'border-image-outset',
-		'-webkit-border-image-repeat',
-		'-moz-border-image-repeat',
-		'-o-border-image-repeat',
 		'border-image-repeat',
-		'-webkit-border-top-image',
-		'-moz-border-top-image',
-		'-o-border-top-image',
 		'border-top-image',
-		'-webkit-border-right-image',
-		'-moz-border-right-image',
-		'-o-border-right-image',
 		'border-right-image',
-		'-webkit-border-bottom-image',
-		'-moz-border-bottom-image',
-		'-o-border-bottom-image',
 		'border-bottom-image',
-		'-webkit-border-left-image',
-		'-moz-border-left-image',
-		'-o-border-left-image',
 		'border-left-image',
-		'-webkit-border-corner-image',
-		'-moz-border-corner-image',
-		'-o-border-corner-image',
 		'border-corner-image',
-		'-webkit-border-top-left-image',
-		'-moz-border-top-left-image',
-		'-o-border-top-left-image',
 		'border-top-left-image',
-		'-webkit-border-top-right-image',
-		'-moz-border-top-right-image',
-		'-o-border-top-right-image',
 		'border-top-right-image',
-		'-webkit-border-bottom-right-image',
-		'-moz-border-bottom-right-image',
-		'-o-border-bottom-right-image',
 		'border-bottom-right-image',
-		'-webkit-border-bottom-left-image',
-		'-moz-border-bottom-left-image',
-		'-o-border-bottom-left-image',
 		'border-bottom-left-image',
 		'background',
 		'filter:progid:DXImageTransform.Microsoft.AlphaImageLoader',
@@ -256,8 +182,6 @@ class CSSqueeze
 		'background-image',
 		'background-attachment',
 		'background-position',
-		'-ms-background-position-x',
-		'-ms-background-position-y',
 		'background-position-x',
 		'background-position-y',
 		'background-clip',
@@ -265,8 +189,6 @@ class CSSqueeze
 		'background-size',
 		'background-repeat',
 		'box-decoration-break',
-		'-webkit-box-shadow',
-		'-moz-box-shadow',
 		'box-shadow',
 		'color',
 		'table-layout',
@@ -283,9 +205,6 @@ class CSSqueeze
 		'-ms-writing-mode',
 		'vertical-align',
 		'text-align',
-		'-webkit-text-align-last',
-		'-moz-text-align-last',
-		'-ms-text-align-last',
 		'text-align-last',
 		'text-decoration',
 		'text-emphasis',
@@ -293,27 +212,19 @@ class CSSqueeze
 		'text-emphasis-style',
 		'text-emphasis-color',
 		'text-indent',
-		'-ms-text-justify',
 		'text-justify',
 		'text-outline',
 		'text-transform',
 		'text-wrap',
-		'-ms-text-overflow',
 		'text-overflow',
 		'text-overflow-ellipsis',
 		'text-overflow-mode',
 		'text-shadow',
 		'white-space',
 		'word-spacing',
-		'-ms-word-wrap',
 		'word-wrap',
-		'-ms-word-break',
 		'word-break',
-		'-moz-tab-size',
-		'-o-tab-size',
 		'tab-size',
-		'-webkit-hyphens',
-		'-moz-hyphens',
 		'hyphens',
 		'letter-spacing',
 		'font',
@@ -327,11 +238,7 @@ class CSSqueeze
 		'src',
 		'line-height',
 		'opacity',
-		'-ms-filter:\'progid:DXImageTransform.Microsoft.Alpha',
 		'filter:progid:DXImageTransform.Microsoft.Alpha(Opacity',
-		'-ms-interpolation-mode',
-		'-webkit-filter',
-		'-ms-filter',
 		'filter',
 		'resize',
 		'cursor',
@@ -340,113 +247,33 @@ class CSSqueeze
 		'nav-right',
 		'nav-down',
 		'nav-left',
-		'-webkit-transition',
-		'-moz-transition',
-		'-ms-transition',
-		'-o-transition',
 		'transition',
-		'-webkit-transition-delay',
-		'-moz-transition-delay',
-		'-ms-transition-delay',
-		'-o-transition-delay',
 		'transition-delay',
-		'-webkit-transition-timing-function',
-		'-moz-transition-timing-function',
-		'-ms-transition-timing-function',
-		'-o-transition-timing-function',
 		'transition-timing-function',
-		'-webkit-transition-duration',
-		'-moz-transition-duration',
-		'-ms-transition-duration',
-		'-o-transition-duration',
 		'transition-duration',
-		'-webkit-transition-property',
-		'-moz-transition-property',
-		'-ms-transition-property',
-		'-o-transition-property',
 		'transition-property',
-		'-webkit-transform',
-		'-moz-transform',
-		'-ms-transform',
-		'-o-transform',
 		'transform',
-		'-webkit-transform-origin',
-		'-moz-transform-origin',
-		'-ms-transform-origin',
-		'-o-transform-origin',
 		'transform-origin',
-		'-webkit-animation',
-		'-moz-animation',
-		'-ms-animation',
-		'-o-animation',
 		'animation',
-		'-webkit-animation-name',
-		'-moz-animation-name',
-		'-ms-animation-name',
-		'-o-animation-name',
 		'animation-name',
-		'-webkit-animation-duration',
-		'-moz-animation-duration',
-		'-ms-animation-duration',
-		'-o-animation-duration',
 		'animation-duration',
-		'-webkit-animation-play-state',
-		'-moz-animation-play-state',
-		'-ms-animation-play-state',
-		'-o-animation-play-state',
 		'animation-play-state',
-		'-webkit-animation-timing-function',
-		'-moz-animation-timing-function',
-		'-ms-animation-timing-function',
-		'-o-animation-timing-function',
 		'animation-timing-function',
-		'-webkit-animation-delay',
-		'-moz-animation-delay',
-		'-ms-animation-delay',
-		'-o-animation-delay',
 		'animation-delay',
-		'-webkit-animation-iteration-count',
-		'-moz-animation-iteration-count',
-		'-ms-animation-iteration-count',
-		'-o-animation-iteration-count',
 		'animation-iteration-count',
-		'-webkit-animation-direction',
-		'-moz-animation-direction',
-		'-ms-animation-direction',
-		'-o-animation-direction',
 		'animation-direction',
 		'pointer-events',
 		'unicode-bidi',
 		'direction',
-		'-webkit-columns',
-		'-moz-columns',
 		'columns',
-		'-webkit-column-span',
-		'-moz-column-span',
 		'column-span',
-		'-webkit-column-width',
-		'-moz-column-width',
 		'column-width',
-		'-webkit-column-count',
-		'-moz-column-count',
 		'column-count',
-		'-webkit-column-fill',
-		'-moz-column-fill',
 		'column-fill',
-		'-webkit-column-gap',
-		'-moz-column-gap',
 		'column-gap',
-		'-webkit-column-rule',
-		'-moz-column-rule',
 		'column-rule',
-		'-webkit-column-rule-width',
-		'-moz-column-rule-width',
 		'column-rule-width',
-		'-webkit-column-rule-style',
-		'-moz-column-rule-style',
 		'column-rule-style',
-		'-webkit-column-rule-color',
-		'-moz-column-rule-color',
 		'column-rule-color',
 		'break-before',
 		'break-inside',
@@ -456,7 +283,6 @@ class CSSqueeze
 		'page-break-after',
 		'orphans',
 		'widows',
-		'-ms-zoom',
 		'zoom',
 		'max-zoom',
 		'min-zoom',
@@ -504,20 +330,45 @@ class CSSqueeze
 	 * $squeezed_css = $parser->squeeze($fat_css);
 	 */
 
-	function squeeze($css, $singleLine = true)
+	function squeeze($css, $singleLine = true, $keepHack = true)
 	{
 		$css = trim($css);
 		if ('' === $css) return '';
 
+		$this->keepHack = $keepHack;
+		$css = $this->prepareComments($css);
+
 		list($selectors, $blocks) = $this->tokenize($css);
 
 		$f = '';
-		foreach($selectors as $k => $v)
+		$tokens = count($selectors);
+		for ($i = 0; $i < $tokens; ++$i)
 		{
-			$f .= $v . '{' . $blocks[$k] . '}';
+			$f .= $selectors[$i] . '{' . $blocks[$i] . '}';
 		}
 
 		return $singleLine ? $this->compress($f) : $this->deflat($f);
+	}
+
+	// from http://minify.googlecode.com/svn/trunk/min/lib/Minify/CSS/Compressor.php
+	protected function prepareComments($css)
+	{
+		if ($this->keepHack)
+		{
+			// preserve empty comment after '>'
+			// http://www.webdevout.net/css-hacks#in_css-selectors
+			$css = preg_replace('@>/\\*\\s*\\*/@', '>/*keep*/', $css);
+
+			// preserve empty comment between property and value
+			// http://css-discuss.incutio.com/?page=BoxModelHack
+			$css = preg_replace('@/\\*\\s*\\*/\\s*:@', '/*keep*/:', $css);
+			$css = preg_replace('@:\\s*/\\*\\s*\\*/@', ':/*keep*/', $css);
+
+			// prevent triggering IE6 bug: http://www.crankygeek.com/ie6pebug/
+			$css = preg_replace('/:first-l(etter|ine)\\{/', ':first-l$1 {', $css);
+		}
+
+		return $css;
 	}
 
 	protected function tokenize($lines)
@@ -548,7 +399,7 @@ class CSSqueeze
 		$selectors = $blocks = array();
 		$spos = $bpos = 0;
 
-		for ($i = 0; $i<$size; ++$i)
+		for ($i = 0; $i < $size; ++$i)
 		{
 			if ($i % 2 == 0)
 			{
@@ -583,10 +434,6 @@ class CSSqueeze
 
 		/* remove empty selectors */
 		$p[] = '#([^}]+){}#isU';
-		$r[] = '';
-
-		/* remove multiline comments */
-		$p[] = '/\/\*.*?\*\//s';
 		$r[] = '';
 
 		/* remove tabs, spaces, newlines, etc. */
