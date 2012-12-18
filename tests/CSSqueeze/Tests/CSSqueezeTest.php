109c46fd208dd602649bdf72f8b087d4c56e5110
Yann Bogdanovic <ianbogda@gmail.com>
2012-12-18 20:48:01 +0100

    + Add phpunit for comments and ws

---
+++ tests/CSSqueeze/Tests/CSSqueezeTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace CSSqueeze\Tests;
+
+use CSSqueeze;
+
+class CSSqueezeTest extends \PHPUnit_Framework_TestCase
+{
+    function testYUICompressor()
+    {
+        if ($h = opendir(__DIR__ . '/test/'))
+        {
+            while ($file = readdir($h))
+            {
+                $xfail = '.xfail' === substr($file, -6) ? '.xfail' : '';
+                if ($xfail) $file = substr($file, 0, -6);
+
+                if ('.css' === substr($file, -4) && file_exists(__DIR__ . '/expected/' . $file))
+                {
+                    $test = file_get_contents(__DIR__ . '/test/' . $file . $xfail);
+                    $expe = file_get_contents(__DIR__ . '/expected/' . $file);
+
+                    $cz = new CSSqueeze;
+                    $test = $cz->squeeze($test) . "\n";
+
+                    $xfail
+                        ? $this->assertFalse($expe === $test, "Xfail {$file}")
+                        : $this->assertSame($expe, $test, "Testing {$file}");
+                }
+            }
+        }
+    }
+}
