d9d960959ec9a5a53e4bdc5cffef07ba32b6e33c
Yann Bogdanovic <ianbogda@gmail.com>
2013-09-11 22:05:53 +0200

    Modify tests process

--- Tests/CSSqueeze/Tests/CSSqueezeTest.php
+++ Tests/CSSqueeze/Tests/CSSqueezeTest.php
@@ -4,28 +4,94 @@ use Patchwork;
 
 class CSSqueezeTest extends \PHPUnit_Framework_TestCase
 {
-    function testCSSqueezeCompressor()
+    function testCSSqueezeRemove()
     {
-        if ($h = opendir(__DIR__ . '/test/'))
+        $xfail= 0;
+        $files = array(
+            'comment.css',
+            'emptyStatment.css',
+            'whitespace.css',
+        );
+
+        foreach($files as $file)
         {
-            while ($file = readdir($h))
-            {
-                $xfail = '.xfail' === substr($file, -6) ? '.xfail' : '';
-                if ($xfail) $file = substr($file, 0, -6);
+            $test = file_get_contents(__DIR__ . '/test/' . $file);
+            $expe = file_get_contents(__DIR__ . '/expected/' . $file);
 
-                if ('.css' === substr($file, -4) && file_exists(__DIR__ . '/expected/' . $file))
-                {
-                    $test = file_get_contents(__DIR__ . '/test/' . $file . $xfail);
-                    $expe = file_get_contents(__DIR__ . '/expected/' . $file);
+            $cz = new CSSqueeze;
+            $test = $cz->squeeze($test) . "\n";
 
-                    $cz = new CSSqueeze;
-                    $test = $cz->squeeze($test) . "\n";
+            $xfail
+                ? $this->assertFalse($expe === $test, "Xfail {$file}")
+                : $this->assertSame($expe, $test, "TestingXfail {$file}");
+        }
+    }
 
-                    $xfail
-                        ? $this->assertFalse($expe === $test, "Xfail {$file}")
-                        : $this->assertSame($expe, $test, "Testing {$file}");
-                }
-            }
+    function testCSSqueezeColors()
+    {
+        $xfail= 0;
+        $files = array(
+            'cutColors.css',
+            'reduceColors.css',
+        );
+
+        foreach($files as $file)
+        {
+            $test = file_get_contents(__DIR__ . '/test/' . $file);
+            $expe = file_get_contents(__DIR__ . '/expected/' . $file);
+
+            $cz = new CSSqueeze;
+            $test = $cz->squeeze($test) . "\n";
+
+            $xfail
+                ? $this->assertFalse($expe === $test, "Xfail {$file}")
+                : $this->assertSame($expe, $test, "TestingXfail {$file}");
+        }
+    }
+
+    function testCSSqueezeHacks()
+    {
+        $xfail= 0;
+        $files = array(
+            'ie6hack.css',
+            'ie7hack.css',
+            'operahack.css',
+        );
+
+        foreach($files as $file)
+        {
+            $test = file_get_contents(__DIR__ . '/test/' . $file);
+            $expe = file_get_contents(__DIR__ . '/expected/' . $file);
+
+            $cz = new CSSqueeze;
+            $test = $cz->squeeze($test) . "\n";
+
+            $xfail
+                ? $this->assertFalse($expe === $test, "Xfail {$file}")
+                : $this->assertSame($expe, $test, "TestingXfail {$file}");
+        }
+    }
+
+    function testCSSqueezesReduce()
+    {
+        $xfail= 0;
+        $files = array(
+            'mergeSameSelectors.css',
+            'mergeselectors.css',
+            'shorthand.css',
+        );
+
+        foreach($files as $file)
+        {
+            $test = file_get_contents(__DIR__ . '/test/' . $file);
+            $expe = file_get_contents(__DIR__ . '/expected/' . $file);
+
+            $cz = new CSSqueeze;
+            $test = $cz->squeeze($test) . "\n";
+
+            $xfail
+                ? $this->assertFalse($expe === $test, "Xfail {$file}")
+                : $this->assertSame($expe, $test, "TestingXfail {$file}");
         }
     }
 }
