9ce2d652b7b3e7668ff9c63638a4be04c8c3545e
ianbogda <ianbogda@gmail.com>
2014-01-21 09:51:34 +0100

    + fix deflat() + add unit tests deflat()

--- Tests/CSSqueeze/Tests/CSSqueezeTest.php
+++ Tests/CSSqueeze/Tests/CSSqueezeTest.php
@@ -93,5 +93,27 @@ class CSSqueezeTest extends \PHPUnit_Framework_TestCase
                 ? $this->assertFalse($expe === $test, "Xfail {$file}")
                 : $this->assertSame($expe, $test, "TestingXfail {$file}");
         }
+	}
+
+    public function testCSSqueezesDeflat()
+    {
+        $xfail= 0;
+        $files = array(
+            'deflat.css',
+            'deflatComments.css',
+        );
+
+        foreach($files as $file)
+        {
+            $test = file_get_contents(__DIR__ . '/test/' . $file);
+            $expe = file_get_contents(__DIR__ . '/expected/' . $file);
+
+            $cz = new CSSqueeze('    ');
+            $test = $cz->squeeze($test, false) . "\n";
+
+            $xfail
+                ? $this->assertFalse($expe === $test, "Xfail {$file}")
+                : $this->assertSame($expe, $test, "TestingXfail {$file}");
+        }
     }
 }
